a7875f60745acb715d76b79691e11a043628e445
Ria Schwalbe <riaschwalbe@Rias-MacBook-Pro-8038.local>
2021-02-24 10:52:48 -0800

    updates

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Twin-Peaks-Trivia
-Self-ScoringTrivia Unit 1 Project
 
+## Description
+If you are a fan of the iconic series Twin Peaks then this 10 question quiz is for you! This is a Self-Scoring Trivia game for my Unit 1 Project
 ## User Stories
-As a player, I would enjoy a fun trivia game that includes a variety of multiple choice questions
+As a player and a fan of the television series Twin Peaks, I would enjoy a fun trivia game that includes a variety of multiple choice questions.
 
 ### MVP
 -as a player, I would like to be informed with some indication when I make a selection so that i know my selection worked
 -as a player, I would like to be informed of the score with a scoreboard
+-as a player, I would like to be informed when my answer is incorrect
 
 #### Silver Stories
 -as a player, I would like to be timed, there should be a clear countdown displayed
@@ -14,6 +16,8 @@ As a player, I would enjoy a fun trivia game that includes a variety of multiple
 ##### Gold Stories
 -as a player, I would like to have an opponent to compete against
 -as a player i would like a victory animation to feel rewarded if i win
+-as a player i would like a visually pleasing trivia game that accomodates both my desktop and mobile device
 
 ###### Wireframe
 <img src="trivia-wireframe.png " alt="">
+Ideally this Trivia game would allow for 2 players with the rush of being timed. With the current MVP status, the layout is slightly different allowing only 1 player, and no time limit.
--- app.js
+++ app.js
@@ -3,104 +3,102 @@ const answersA = document.querySelector(".answerOptionsA")
 const answersB = document.querySelector(".answerOptionsB")
 const answersC = document.querySelector(".answerOptionsC")
 const triviaContainer = document.querySelector(".triviaResults")
+const nextBtn = document.querySelector(".nextBtn");
 
 let currentAnswer = " "
-let answerSelected= false
+let answerSelected = false
 
 const triviaQuestions = [{
-    question: "What is The Log made of?",
-    answers: {
-        a: "Douglas fir",
-        b: "Redwood",
-        c: "Ponderosa Pine"
+        question: "What is The Log made of?",
+        answers: {
+            a: "Douglas fir",
+            b: "Redwood",
+            c: "Ponderosa Pine"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-},
-{
-    question: "Who killed Laura Palmer?",
-    answers: {
-        a: "Leo Johnson",
-        b: "Bob",
-        c: "Bobby"
+    {
+        question: "Who killed Laura Palmer?",
+        answers: {
+            a: "Leo Johnson",
+            b: "Bob",
+            c: "Bobby"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
-{
-    question: "Don’t drink the coffee, a ____ was found in the percolator!",
-    answers: {
-        a: "Fish",
-        b: "Cockroach",
-        c: "Fingernail"
+    {
+        question: "Don’t drink the coffee, a ____ was found in the percolator!",
+        answers: {
+            a: "Fish",
+            b: "Cockroach",
+            c: "Fingernail"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-},
-{
-    question: "What is the name of the casino girls have gone missing from?",
-    answers: {
-        a: "Smokey Poker",
-        b: "One-Eyed Jack's",
-        c: "Dealer's Yard"
+    {
+        question: "What is the name of the casino girls have gone missing from?",
+        answers: {
+            a: "Smokey Poker",
+            b: "One-Eyed Jack's",
+            c: "Dealer's Yard"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
-{
-    question: "What famous diretor almost directed the second season in place of David Lynch?",
-    answers: {
-        a: "Stanley Kubrick",
-        b: "Steven Spielberg",
-        c: "Wes Anderson"
+    {
+        question: "What famous diretor almost directed the second season in place of David Lynch?",
+        answers: {
+            a: "Stanley Kubrick",
+            b: "Steven Spielberg",
+            c: "Wes Anderson"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
-{
-    question: "The ____ are not what they seem.",
-    answers: {
-        a: "owls",
-        b: "donuts",
-        c: "murders"
+    {
+        question: "The ____ are not what they seem.",
+        answers: {
+            a: "owls",
+            b: "donuts",
+            c: "murders"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-},
-{
-    question: "Which character was a homage to the 1993 film The Fugitive?",
-    answers: {
-        a: "The Giant",
-        b: "Leo Johnson",
-        c: "The One armed man"
+    {
+        question: "Which character was a homage to the 1993 film The Fugitive?",
+        answers: {
+            a: "The Giant",
+            b: "Leo Johnson",
+            c: "The One armed man"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-},
-{
-    question: "Which character was spontaneously cast from the crew?",
-    answers: {
-        a: "Jean Renault",
-        b: "Agent Rosenfield",
-        c: "Bob"
+    {
+        question: "Which character was spontaneously cast from the crew?",
+        answers: {
+            a: "Jean Renault",
+            b: "Agent Rosenfield",
+            c: "Bob"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-},
-{
-    question: "What David Lynch film featured the same floor from the Red Room?",
-    answers: {
-        a: "Eraserhead",
-        b: "Lost Highway",
-        c: "Mulholland Drive"
+    {
+        question: "What David Lynch film featured the same floor from the Red Room?",
+        answers: {
+            a: "Eraserhead",
+            b: "Lost Highway",
+            c: "Mulholland Drive"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-},
-{
-    question: "The conspiracies surrounding what famous actress’s death influenced the show?",
-    answers: {
-        a: "Natalie Wood,",
-        b: "Brittany Murphy",
-        c: "Marilyn Monroe"
-    },
-    correctAnswer: "c"
-}
+    {
+        question: "The conspiracies surrounding what famous actress’s death influenced the show?",
+        answers: {
+            a: "Natalie Wood,",
+            b: "Brittany Murphy",
+            c: "Marilyn Monroe"
+        },
+        correctAnswer: "c"
+    }
 ];
-// console.log(triviaQuestions[0].question)
-// console.log(triviaQuestions)
-// console.log(triviaQuestions[0].answers)
 
 let score = 0; //game starts with a score of 0
 let currentQuestionIndex = 0; //trivia is sorted in an array
@@ -110,68 +108,66 @@ answersA.innerText = triviaQuestions[currentQuestionIndex].answers.a
 answersB.innerText = triviaQuestions[currentQuestionIndex].answers.b
 answersC.innerText = triviaQuestions[currentQuestionIndex].answers.c
 
-const nextBtn = document.querySelector(".nextBtn");
-
 nextBtn.addEventListener("click", (event) => {
     event.preventDefault;
     answerSelected = false //once nxtBtn is selected the answerSelected   returns to false (answerSeleced becomes true when answers a-c are chosen)
     currentQuestionIndex++;
     //if last question is on screen hide nextBtn
-    if(currentQuestionIndex === 9){
+    if (currentQuestionIndex === 9) {
         nextBtn.style.display = 'none'
-    }  
-    document.getElementById("wrong").innerText= "";
+    }
+    document.getElementById("wrong").innerText = "";
     //   increment question index & load next question w/its set of answers
     question.innerText = triviaQuestions[currentQuestionIndex].question
     answersA.innerText = triviaQuestions[currentQuestionIndex].answers.a
     answersB.innerText = triviaQuestions[currentQuestionIndex].answers.b
     answersC.innerText = triviaQuestions[currentQuestionIndex].answers.c
-    
+
 });
 
 answersA.addEventListener("click", (event) => {
     event.preventDefault;
-    if(answerSelected){ //return the info below if this answer is selected
+    if (answerSelected) { //return the info below if this answer is selected
         return
     }
     currentAnswer = "a"
     if (currentAnswer === triviaQuestions[currentQuestionIndex].correctAnswer) {
-        score += 10; 
+        score += 10;
         //if correct answer is selected ad 10pts to score & announce score
-        document.getElementById("score").innerText= "Score : " + score;       
-    } else{
+        document.getElementById("score").innerText = "Score : " + score;
+    } else {
         // if wrong answer selected generate wrong answer response
-        document.getElementById("wrong").innerText= "Wrong Answer";
+        document.getElementById("wrong").innerText = "Wrong Answer";
     }
     answerSelected = true
 });
 
 answersB.addEventListener("click", (event) => {
     event.preventDefault;
-    if(answerSelected){
+    if (answerSelected) {
         return
     }
     currentAnswer = "b"
     if (currentAnswer === triviaQuestions[currentQuestionIndex].correctAnswer) {
         score += 10;
-        document.getElementById("score").innerText= "Score : " + score;    
-    } else{
-        document.getElementById("wrong").innerText= "Wrong Answer";
+        document.getElementById("score").innerText = "Score : " + score;
+    } else {
+        document.getElementById("wrong").innerText = "Wrong Answer";
     }
     answerSelected = true
 });
 
 answersC.addEventListener("click", (event) => {
     event.preventDefault;
-    if(answerSelected){
+    if (answerSelected) {
         return
     }
     currentAnswer = "c"
     if (currentAnswer === triviaQuestions[currentQuestionIndex].correctAnswer) {
         score += 10;
-        document.getElementById("score").innerText= "Score : " + score;   
-    } else{ 
-        document.getElementById("wrong").innerText= "Wrong Answer";
+        document.getElementById("score").innerText = "Score : " + score;
+    } else {
+        document.getElementById("wrong").innerText = "Wrong Answer";
     }
     answerSelected = true
 });
@@ -179,52 +175,25 @@ answersC.addEventListener("click", (event) => {
 triviaContainer.addEventListener("click", (event) => {
     event.preventDefault;
     //once answer is selected on last question
-    if(currentQuestionIndex === 9){
-        triviaContainer.style.display = 'none'// remove current question & answers from triviaContainer
-        gameOver () // and display GAME OVER & score
-    }    
+    if (currentQuestionIndex === 9) {
+        triviaContainer.style.display = 'none' // remove current question & answers from triviaContainer
+        gameOver() // and display GAME OVER & score
+    }
 });
 
-console.log(score)
-
-
 //SCORE DISPLAY
-function gameOver () {
-    if(score === 100){ //do i need to add innerText for this to work?
-        document.querySelector(".board").innerText= score + " points: Now that's a damn fine perfect score!";
+function gameOver() {
+    if (score === 100) { //response based on score
+        document.querySelector(".board").innerText = score + " points: Now that's a damn fine perfect score!";
         //TP-understand.jpg
-    } else if (score >= 60){
-        document.querySelector(".board").innerText= score + " points: Now that's a damn fine effort!";//massive-quantities.jpg
-    } else if (score >= 40){
-        document.querySelector(".board").innerText= score  + " points: Maybe it's time for a rewatch!";//thumbs-up.jpeg
+    } else if (score >= 60) {
+        document.querySelector(".board").innerText = score + " points: Now that's a damn fine effort!"; //massive-quantities.jpg
+    } else if (score >= 40) {
+        document.querySelector(".board").innerText = score + " points: Maybe it's time for a rewatch!"; //thumbs-up.jpeg
     } else {
-        document.querySelector(".board").innerText= score  + " points: Time to start Twin Peaks!"; //low-score.jpg
+        document.querySelector(".board").innerText = score + " points: Time to start watching Twin Peaks!"; //low-score.jpg
     }
 }
-
-
-
-//generate new button to start over?
-
-//Twin Peaks font source https://halloweenlove.com/twin-peaks-font-style-download/
-// function pleaseWork() {
-//     // currentQuestionIndex++
-//     question.innerText = triviaQuestions[currentQuestionIndex].question
-//     answersA.innerText = triviaQuestions[currentQuestionIndex].answers.a
-//     answersB.innerText = triviaQuestions[currentQuestionIndex].answers.b
-//     answersC.innerText = triviaQuestions[currentQuestionIndex].answers.c
-    
-    
-    
-// }
-// console.log(triviaQuestions[currentQuestionIndex].correctAnswer)
-
-
-
-
-
-
-
 
 // /*
 // //create div for trivia questions & answers
@@ -237,12 +206,10 @@ function gameOver () {
 
 // //determine if theanswer selected resulted in a correct answer
 
-// //const player = "x"
-
 // //syntax for trivia questions, use object literals to display questions in
 // // an array
 
-// source used https://www.sitepoint.com/simple-javascript-quiz/
+// source used  for triviaQuestions array https://www.sitepoint.com/simple-javascript-quiz/
 
 // //use a data property like data-id='0' ect
 // // to figure out which box was clicked
@@ -251,49 +218,4 @@ function gameOver () {
 // //use an if/else to check ea winning combo
 // //starting point, but something cleaner can be 
 // //done??
-
-// ---Trivia Questions---
-// What is The Log made of?
-// Correct answer- Ponderosa Pine 
-// Wrong answers- douglas fir, pine, redwood
-
-// 2. Who killed Laura Palmer?
-// Correct- Bob
-// Wrong-  Leo Johnson, Bobby, Donna Hayward
-
-// 3. Don’t drink the coffee, a ____ was found in the percolator!
-// Correct answer- fish
-// Wrong answers-  cockroach, finger nail, sedative
-
-// 4. What is the name of the casino girls have gone missing from?
-// Correct answer- One-Eyed Jack’s
-// Wrong answers- Smoky Poker, Lucky Hunch, Dealer Yard
-
-// 5. Who almost directed the second season in place of David Lynch?
-// Correct answer- Steve Spielberg
-// Wrong answers- Stanley Kubrick, Tim Burton, Wes Anderson
-
-// 6. Entertainment Weekly included this show in its “25 Greatest Cult TV Shows Ever” in 2009 listed at number…?
-// Correct answer- 5
-// Wrong answers- 1, 25, 20
-
-// 7. Which character was a homage to the 1993 film The Fugitive?
-// Correct answer- The One armed man
-// Wrong answers- The Giant, Dr. Jacoby, Leo Johnson
-
-// 8. Which character was spontaneously cast from the crew?
-// Correct answer- Bob
-// Wrong answers- Josie Packard, Jean Renault, Agent Rosenfield
-// 9. What David Lynch film featured the same floor from the Red Room?
-// Correct answer- Eraserhead
-// Wrong answers- Mulholland Drive, Lost Highway, Blue Velvet
-
-// 10. The conspiracies surrounding what famous actress’s death influenced the show?
-// Correct answer- Marilyn Monroe
-// Wrong answers- Natalie Wood, Thelma Todd, Brittany Murphy
-
-
-
-
-
 // */
